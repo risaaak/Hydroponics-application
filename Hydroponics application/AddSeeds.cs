cff0f24590a1f43bd4b50f7b4ea05988bceb2243
isaak.banquiles@hotmail.com <104627649+risaaak@users.noreply.github.com>
2022-11-23 17:07:48 +0800

    Add sendDataToDatabase function

--- Hydroponics application/AddSeeds.cs
+++ Hydroponics application/AddSeeds.cs
@@ -20,18 +20,8 @@ namespace Hydroponics_application
         public string connectionString = "Data Source=MY-DESKTOP\\SQLEXPRESS;Initial Catalog=HYDROPONICS_TEST;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
         private void Addbtn_Click(object sender, EventArgs e)
         {
-            SqlConnection con = new SqlConnection(connectionString);
-            con.Open();
-            try
-            {
-                SqlCommand command = new SqlCommand("INSERT INTO SEEDS (seed_name) VALUES(@seedName)", con);
-                command.Parameters.Add(new SqlParameter("seedName", textBox1.Text));
-                command.ExecuteNonQuery();
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
+            string seedName = textBox1.Text;
+            sendDataToDatabase(seedName);
         }
 
         private void UpdateBtn_Click(object sender, EventArgs e)
@@ -39,6 +29,26 @@ namespace Hydroponics_application
             updateSeeds updateSeeds = new updateSeeds();
             updateSeeds.Show();
             this.Hide();
+        }
+
+        private void sendDataToDatabase(string seedname, int timesPlanted=0, int timesSprouted=0, float germinationRate=0)
+        {
+            SqlConnection con = new SqlConnection(connectionString);
+            con.Open();
+            try
+            {
+                SqlCommand command = new SqlCommand("INSERT INTO SEEDS VALUES(@seedName, @timesPlanted, @timesSprouted, @germinationRate)", con);
+                command.Parameters.Add(new SqlParameter("seedName", seedname));
+                command.Parameters.Add(new SqlParameter("timesPlanted", timesPlanted));
+                command.Parameters.Add(new SqlParameter("timesSprouted", timesSprouted));
+                command.Parameters.Add(new SqlParameter("germinationRate", germinationRate));
+                command.ExecuteNonQuery();
+                MessageBox.Show("Added Successfully!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
