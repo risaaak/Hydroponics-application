5c5d69115d88bb7214d52a76c39d1afc868bf6c9
isaak.banquiles@hotmail.com <104627649+risaaak@users.noreply.github.com>
2023-08-18 16:11:17 +0800

    Add addExpenditureForm

---
+++ Hydroponics application/addExpenditureForm.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace Hydroponics_application
+{
+    partial class addExpenditureForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.backButton = new System.Windows.Forms.Button();
+            this.itemNameTextbox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.itemQuantityTextbox = new System.Windows.Forms.TextBox();
+            this.itemPriceTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.itemDescriptionTextbox = new System.Windows.Forms.TextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.Date = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.addButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // backButton
+            // 
+            this.backButton.Location = new System.Drawing.Point(13, 13);
+            this.backButton.Name = "backButton";
+            this.backButton.Size = new System.Drawing.Size(75, 23);
+            this.backButton.TabIndex = 0;
+            this.backButton.Text = "Back";
+            this.backButton.UseVisualStyleBackColor = true;
+            this.backButton.Click += new System.EventHandler(this.backButton_Click);
+            // 
+            // itemNameTextbox
+            // 
+            this.itemNameTextbox.Location = new System.Drawing.Point(66, 83);
+            this.itemNameTextbox.Name = "itemNameTextbox";
+            this.itemNameTextbox.Size = new System.Drawing.Size(221, 20);
+            this.itemNameTextbox.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(66, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Item Name";
+            // 
+            // itemQuantityTextbox
+            // 
+            this.itemQuantityTextbox.Location = new System.Drawing.Point(66, 137);
+            this.itemQuantityTextbox.Name = "itemQuantityTextbox";
+            this.itemQuantityTextbox.Size = new System.Drawing.Size(100, 20);
+            this.itemQuantityTextbox.TabIndex = 3;
+            // 
+            // itemPriceTextBox
+            // 
+            this.itemPriceTextBox.Location = new System.Drawing.Point(192, 136);
+            this.itemPriceTextBox.Name = "itemPriceTextBox";
+            this.itemPriceTextBox.Size = new System.Drawing.Size(100, 20);
+            this.itemPriceTextBox.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(66, 121);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Item Quantity";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(192, 117);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Price Per Piece";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(66, 177);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(83, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Item Description";
+            // 
+            // itemDescriptionTextbox
+            // 
+            this.itemDescriptionTextbox.Location = new System.Drawing.Point(69, 194);
+            this.itemDescriptionTextbox.Multiline = true;
+            this.itemDescriptionTextbox.Name = "itemDescriptionTextbox";
+            this.itemDescriptionTextbox.Size = new System.Drawing.Size(223, 56);
+            this.itemDescriptionTextbox.TabIndex = 8;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePicker1.Location = new System.Drawing.Point(471, 79);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 22);
+            this.dateTimePicker1.TabIndex = 9;
+            // 
+            // Date
+            // 
+            this.Date.AutoSize = true;
+            this.Date.Location = new System.Drawing.Point(471, 58);
+            this.Date.Name = "Date";
+            this.Date.Size = new System.Drawing.Size(35, 13);
+            this.Date.TabIndex = 10;
+            this.Date.Text = "label5";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(13, 328);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(775, 215);
+            this.dataGridView1.TabIndex = 11;
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(339, 275);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(94, 30);
+            this.addButton.TabIndex = 12;
+            this.addButton.Text = "Add";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // addExpenditureForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 555);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.Date);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.itemDescriptionTextbox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.itemPriceTextBox);
+            this.Controls.Add(this.itemQuantityTextbox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.itemNameTextbox);
+            this.Controls.Add(this.backButton);
+            this.Name = "addExpenditureForm";
+            this.Text = "addExpenditureForm";
+            this.Load += new System.EventHandler(this.addExpenditureForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button backButton;
+        private System.Windows.Forms.TextBox itemNameTextbox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox itemQuantityTextbox;
+        private System.Windows.Forms.TextBox itemPriceTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox itemDescriptionTextbox;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label Date;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button addButton;
+    }
+}
---
+++ Hydroponics application/addExpenditureForm.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hydroponics_application
+{
+    public partial class addExpenditureForm : Form
+    {
+        public Form RefToMainForm { get; set; }
+
+        public string connectionString = "Data Source=MY-DESKTOP\\SQLEXPRESS;Initial Catalog=HYDROPONICS_TEST;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        public addExpenditureForm()
+        {
+            InitializeComponent();
+        }
+
+        public void loadTable()
+        {
+            SqlConnection con = new SqlConnection(connectionString);
+            SqlCommand loadTable = new SqlCommand("SELECT * FROM EXPENDITURES", con);
+            SqlDataAdapter dataAdapter = new SqlDataAdapter();
+            dataAdapter.SelectCommand = loadTable;
+            DataTable table = new DataTable();
+            table.Clear();
+            dataAdapter.Fill(table);
+            dataGridView1.DataSource = table;
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void addExpenditureForm_Load(object sender, EventArgs e)
+        {
+            loadTable();
+        }
+
+        private void backButton_Click(object sender, EventArgs e)
+        {
+            this.RefToMainForm.Show();
+            this.Close();
+        }
+    }
+}
