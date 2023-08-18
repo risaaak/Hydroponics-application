4ccfc9f5071968ff04cb90675cbd555a09b35ef7
isaak.banquiles@hotmail.com <104627649+risaaak@users.noreply.github.com>
2023-08-18 13:32:25 +0800

    Add new Income Form

---
+++ Hydroponics application/AddIncomeForm.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace Hydroponics_application
+{
+    partial class AddIncomeForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.addButton = new System.Windows.Forms.Button();
+            this.productDescriptionBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.backButton = new System.Windows.Forms.Button();
+            this.productNameTextBox = new System.Windows.Forms.TextBox();
+            this.quantityTextbox = new System.Windows.Forms.TextBox();
+            this.pricePerPieceTextbox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(311, 266);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(120, 23);
+            this.addButton.TabIndex = 0;
+            this.addButton.Text = "Add";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // productDescriptionBox
+            // 
+            this.productDescriptionBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.productDescriptionBox.Location = new System.Drawing.Point(82, 176);
+            this.productDescriptionBox.Multiline = true;
+            this.productDescriptionBox.Name = "productDescriptionBox";
+            this.productDescriptionBox.Size = new System.Drawing.Size(263, 61);
+            this.productDescriptionBox.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(79, 160);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Income Description";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePicker1.Location = new System.Drawing.Point(497, 64);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(148, 22);
+            this.dateTimePicker1.TabIndex = 3;
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(494, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Date";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 329);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(776, 280);
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // backButton
+            // 
+            this.backButton.Location = new System.Drawing.Point(13, 13);
+            this.backButton.Name = "backButton";
+            this.backButton.Size = new System.Drawing.Size(75, 23);
+            this.backButton.TabIndex = 6;
+            this.backButton.Text = "Back";
+            this.backButton.UseVisualStyleBackColor = true;
+            this.backButton.Click += new System.EventHandler(this.backButton_Click);
+            // 
+            // productNameTextBox
+            // 
+            this.productNameTextBox.Location = new System.Drawing.Point(82, 68);
+            this.productNameTextBox.Name = "productNameTextBox";
+            this.productNameTextBox.Size = new System.Drawing.Size(263, 20);
+            this.productNameTextBox.TabIndex = 7;
+            this.productNameTextBox.TextChanged += new System.EventHandler(this.productNameTextBox_TextChanged);
+            // 
+            // quantityTextbox
+            // 
+            this.quantityTextbox.Location = new System.Drawing.Point(82, 115);
+            this.quantityTextbox.Name = "quantityTextbox";
+            this.quantityTextbox.Size = new System.Drawing.Size(115, 20);
+            this.quantityTextbox.TabIndex = 8;
+            this.quantityTextbox.TextChanged += new System.EventHandler(this.quantityTextbox_TextChanged);
+            this.quantityTextbox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.quantityTextbox_KeyPress);
+            // 
+            // pricePerPieceTextbox
+            // 
+            this.pricePerPieceTextbox.Location = new System.Drawing.Point(226, 115);
+            this.pricePerPieceTextbox.Name = "pricePerPieceTextbox";
+            this.pricePerPieceTextbox.Size = new System.Drawing.Size(119, 20);
+            this.pricePerPieceTextbox.TabIndex = 9;
+            this.pricePerPieceTextbox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.pricePerPiece_Keypress);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(82, 43);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Product Name";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(82, 96);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Quantity";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(223, 96);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(79, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Price per Piece";
+            // 
+            // AddIncomeForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 621);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.pricePerPieceTextbox);
+            this.Controls.Add(this.quantityTextbox);
+            this.Controls.Add(this.productNameTextBox);
+            this.Controls.Add(this.backButton);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.productDescriptionBox);
+            this.Controls.Add(this.addButton);
+            this.Name = "AddIncomeForm";
+            this.Text = "AddIncomeForm";
+            this.Load += new System.EventHandler(this.AddIncomeForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.TextBox productDescriptionBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button backButton;
+        private System.Windows.Forms.TextBox productNameTextBox;
+        private System.Windows.Forms.TextBox quantityTextbox;
+        private System.Windows.Forms.TextBox pricePerPieceTextbox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
---
+++ Hydroponics application/AddIncomeForm.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hydroponics_application
+{
+    public partial class AddIncomeForm : Form
+    {
+        public Form RefToMainForm { get; set; }
+        public AddIncomeForm()
+        {
+            InitializeComponent();
+        }
+        int quantity;
+        double price, totalAmount;
+        string name, description;
+        DateTime date;
+        public string connectionString = "Data Source=MY-DESKTOP\\SQLEXPRESS;Initial Catalog=HYDROPONICS_TEST;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+        public void loadTable()
+        {
+            SqlConnection con = new SqlConnection(connectionString);
+            SqlCommand loadTable = new SqlCommand("SELECT * FROM INCOME", con);
+            SqlDataAdapter dataAdapter = new SqlDataAdapter();
+            dataAdapter.SelectCommand = loadTable;
+            DataTable dt = new DataTable();
+            dt.Clear();
+            dataAdapter.Fill(dt);
+            dataGridView1.DataSource = dt;
+        }
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            name = productNameTextBox.Text;
+            description = productDescriptionBox.Text;
+            quantity = Convert.ToInt32(quantityTextbox.Text);
+            price = Convert.ToDouble(pricePerPieceTextbox.Text);
+            date = dateTimePicker1.Value;
+            totalAmount = getTotalAmount(quantity,price);
+            SqlConnection con = new SqlConnection(connectionString);
+            SqlCommand sendDataToDatabase = new SqlCommand("INSERT INTO INCOME VALUES(@incomeDescription, @name, @quantity, @pricePerPiece," +
+                " @totalAmount, @incomeDate)",con);
+            sendDataToDatabase.Parameters.AddWithValue("name", name);
+            sendDataToDatabase.Parameters.AddWithValue ("incomeDescription", description);
+            sendDataToDatabase.Parameters.AddWithValue("quantity", quantity);
+            sendDataToDatabase.Parameters.AddWithValue("pricePerPiece", price);
+            sendDataToDatabase.Parameters.AddWithValue("totalAmount", totalAmount);
+            sendDataToDatabase.Parameters.AddWithValue("incomeDate", date);
+            con.Open();
+            sendDataToDatabase.ExecuteNonQuery();
+            con.Close();
+            loadTable();
+        }
+        public double getTotalAmount(int quantity, double totalAmount)
+        {
+            totalAmount = quantity * totalAmount;
+            return totalAmount;
+        }
+        private void AddIncomeForm_Load(object sender, EventArgs e)
+        {
+            loadTable();
+        }
+
+        private void backButton_Click(object sender, EventArgs e)
+        {
+            this.RefToMainForm.Show();
+            this.Close();
+        }
+
+        private void quantityTextbox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void productNameTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void quantityTextbox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar >= '0' && e.KeyChar <= '9' )
+            {
+                e.Handled = false; //Do not reject the input
+            }
+            else if (e.KeyChar == ((char)Keys.Back)) //backspace
+            {
+                e.Handled = false;
+            }
+            else
+            {
+                e.Handled = true; //Reject the input
+            }
+        }
+
+        private void pricePerPiece_Keypress(object sender, KeyPressEventArgs e)
+        {
+            if (Char.IsDigit(e.KeyChar)) return;
+            if (Char.IsControl(e.KeyChar)) return;
+            if ((e.KeyChar == '.') && ((sender as TextBox).Text.Contains('.') == false)) return;
+            if ((e.KeyChar == '.') && ((sender as TextBox).SelectionLength == (sender as TextBox).TextLength)) return;
+            e.Handled = true;
+        }
+    }
+}
+
